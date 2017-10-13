00f40cedd7d23c1b8441063d702175096ae4863a
huuduy216 <duyquay123@yahoo.com.vn>
2017-10-13 18:39:06 -0400

    Home page config
    Merge remote-tracking branch 'origin/master'
    Test path fix

--- src/com/harvard/config/Configuration.java
+++ src/com/harvard/config/Configuration.java
@@ -5,10 +5,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Configuration {
 	private static final String WINDOWS_URL_CONFIG_PATH = System.getProperty("user.home") + "\\Desktop\\url.txt";
-	private static final String MAC_URL_CONFIG_PATH = System.getProperty("user.home") + "\\Desktop\\url.txt";
+	private static final String MAC_URL_CONFIG_PATH = "/Users/huitlabadmin/Desktop/url.txt";
+	private static final String WINDOWS_HOMEPAGE_CONFIG_PATH = System.getProperty("user.home") + "\\Desktop\\homepage.txt";
 	private static final String OS_NAME = System.getProperty("os.name");
 
 	/*
@@ -19,6 +22,7 @@ public class Configuration {
 		List<String[]> result = new ArrayList<String[]>();
 
 		if (OS_NAME.contains("Mac")) {
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
 			path = MAC_URL_CONFIG_PATH;
 		} else if (OS_NAME.contains("Window")) {
 			path = WINDOWS_URL_CONFIG_PATH;
@@ -38,5 +42,47 @@ public class Configuration {
 			e.printStackTrace();
 		}
 		return result;
+	}
+	
+	public static String getHomePage() {
+		String path = "";
+		String result = "";
+
+		if (OS_NAME.contains("Mac")) {
+			path = MAC_URL_CONFIG_PATH;
+		} else if (OS_NAME.contains("Window")) {
+			path = WINDOWS_HOMEPAGE_CONFIG_PATH;
+		}
+		
+		try (BufferedReader urlReader = new BufferedReader(new FileReader(path))) {
+
+			String currentLine;
+
+			while ((currentLine = urlReader.readLine()) != null) {
+				result = currentLine;
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Home page: "+result);
+		return result;
+	}
+	
+	public static String getComputerName() {
+
+		String hostname = "Unknown";
+
+		try
+		{
+		    InetAddress addr;
+		    addr = InetAddress.getLocalHost();
+		    hostname = addr.getHostName();
+		}
+		catch (UnknownHostException ex)
+		{
+		    System.out.println("Hostname can not be resolved");
+		}
+		return hostname;
 	}
 }
